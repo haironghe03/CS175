bc878454cebd6a0826073a0ba519dab644704867
haironghe03 <145037897+haironghe03@users.noreply.github.com>
2024-05-21 12:33:16 -0400

    Add files via upload

---
+++ build/cs175-asst3.build/Debug/cs175-asst3.build/Objects-normal/arm64/asst3.d
@@ -0,0 +1,9 @@
+dependencies: \
+  /Users/wangxilin/Desktop/cs175/assignment-3-helenandbozhen/asst3.cpp \
+  /Users/wangxilin/Desktop/cs175/assignment-3-helenandbozhen/GL/glew.h \
+  /Users/wangxilin/Desktop/cs175/assignment-3-helenandbozhen/GL/glfw3.h \
+  /Users/wangxilin/Desktop/cs175/assignment-3-helenandbozhen/cvec.h \
+  /Users/wangxilin/Desktop/cs175/assignment-3-helenandbozhen/geometrymaker.h \
+  /Users/wangxilin/Desktop/cs175/assignment-3-helenandbozhen/glsupport.h \
+  /Users/wangxilin/Desktop/cs175/assignment-3-helenandbozhen/matrix4.h \
+  /Users/wangxilin/Desktop/cs175/assignment-3-helenandbozhen/ppm.h
---
+++ build/cs175-asst3.build/Debug/cs175-asst3.build/Objects-normal/arm64/ppm.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/wangxilin/Desktop/cs175/assignment-3-helenandbozhen/ppm.cpp \
+  /Users/wangxilin/Desktop/cs175/assignment-3-helenandbozhen/GL/glew.h \
+  /Users/wangxilin/Desktop/cs175/assignment-3-helenandbozhen/GL/glfw3.h \
+  /Users/wangxilin/Desktop/cs175/assignment-3-helenandbozhen/ppm.h
